3eb237af55e448c1078431a951367c287ec7a892
David Ghazi <davidaghazi@gmail.com>
2021-03-31 23:36:44 -0400

    completed our approach section with responsiveness

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import BigBanner from "./BigBanner";
 import OurMission from "./OurMission";
+import OurApproach from "./OurApproach";
 import OurCoreWork from "./OurCoreWork";
 
 export default function LandingPage() {
@@ -9,6 +10,7 @@ export default function LandingPage() {
     <>
       <BigBanner />
       <OurMission />
+      <OurApproach />
       <OurCoreWork />
     </>
   );
