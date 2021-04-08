7ba69bc863d62df6b717a8295ac36ccf7b3b39d6
Mikolaj Kaliszewski <kaliszewski.mikolaj@gmail.com>
2021-04-08 09:29:29 +0200

    impr:change hero structure

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,12 +2,11 @@ import React from "react";
 import Box from "@material-ui/core/Box";
 import { GatsbyImage } from "gatsby-plugin-image";
 
-import HeroTitle from "components/Hero/HeroTitle";
 import "styles/hero.css";
 
 const Hero = ({ image, alt, title }) => (
   <Box className="hero">
-    <HeroTitle title={title} />
+    <div className="hero__title">{title}</div>
     <GatsbyImage image={image} alt={alt} className="hero__image" />
   </Box>
 );
