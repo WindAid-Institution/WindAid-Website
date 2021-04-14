bf4b380f3b11e6587d34385bd44b3a66bcabfcf6
Mikolaj Kaliszewski <kaliszewski.mikolaj@gmail.com>
2021-04-14 18:06:31 +0200

    feat: reimplement hero styles

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,15 +2,59 @@ import React from "react";
 import Box from "@material-ui/core/Box";
 import { GatsbyImage } from "gatsby-plugin-image";
 import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
 
-import "styles/hero.css";
+const useStyles = makeStyles((theme) => ({
+  root: { position: "relative" },
+  image: {
+    height: "calc(19.7vw + 329px)",
+    maxHeight: "613px",
+  },
+  title: {
+    position: "absolute",
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    top: "50%",
+    transform: "translateY(-50%)",
+    width: "200px",
+    height: "67px",
+    paddingRight: "16px",
+    zIndex: 99,
+    color: theme.palette.secondary.main,
+    background: "rgba(0, 0, 0, 0.4)",
+    fontWeight: 700,
+    fontSize: "24px",
+    lineHeight: "35px",
 
-const Hero = ({ image, alt, title }) => (
-  <Box className="hero">
-    <Box className="hero__title">{title}</Box>
-    <GatsbyImage image={image} alt={alt} className="hero__image" />
-  </Box>
-);
+    [theme.breakpoints.up("sm")]: {
+      width: "calc(0.251 * 100vw + 49.4px)",
+      height: "calc(0.061 * 100vw + 30.4px)",
+      paddingRight: "calc(0.019 * 100vw + 4.6px)",
+      fontSize: "calc(0.02 * 100vw + 12px)",
+      lineHeight: "calc(0.0226 * 100vw + 21.4px)",
+    },
+
+    [theme.breakpoints.up("xl")]: {
+      width: "411px",
+      height: "118px",
+      paddingRight: "32px",
+      fontSize: "40px",
+      lineHeight: "54px",
+    },
+  },
+}));
+
+const Hero = ({ image, alt, title }) => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.title}>{title}</Box>
+      <GatsbyImage image={image} alt={alt} className={classes.image} />
+    </Box>
+  );
+};
 
 Hero.propTypes = {
   alt: PropTypes.string.isRequired,
--- src/components/Home/BigBanner.js
+++ src/components/Home/BigBanner.js
@@ -1,28 +1,52 @@
 import React from "react";
 import { GatsbyImage } from "gatsby-plugin-image";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
 import useHomepageData from "src/hooks/queries/homepage";
 
-import "../../styles/banner.css";
+const useStyles = makeStyles((theme) => ({
+  root: { position: "relative", height: "100%", width: "100%" },
+  picture: {
+    height: "calc(19.7vw + 329px)",
+    maxHeight: "613px",
+    width: "100%",
+    objectFit: "cover",
+  },
+  heading: {
+    position: "absolute",
+    top: "calc(4.17vw + 33px)",
+    left: "calc(16.9vw - 45px)",
+    color: theme.palette.secondary.main,
+    fontSize: "calc(1.48vw + 18.7px)",
+    fontWeight: theme.typography.fontWeightBold,
+    lineHeight: "calc(1.76vw + 28.7px)",
+    width: "calc(48.1vw + 155px)",
+  },
+}));
 
 const BigBanner = () => {
+  const classes = useStyles();
   const {
     hero: {
       title,
       image: { description, gatsbyImageData },
     },
   } = useHomepageData();
+
   return (
-    <Grid container className="banner-container">
-      <Grid item className="banner-container">
+    <Grid container className={classes.root}>
+      <Grid item className={classes.root}>
         <GatsbyImage
           image={gatsbyImageData}
-          className="banner-pic"
+          className={classes.picture}
           alt={description}
           backgroundColor="#056839"
         />
-        <div className="banner-text">{title}</div>
+        <Typography variant="h1" className={classes.heading}>
+          {title}
+        </Typography>
       </Grid>
     </Grid>
   );
