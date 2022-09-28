981cec276e3605a285d4874f8f9cd873a790cea4
Rishi Khan <rishi.rkhan@gmail.com>
2022-09-27 23:21:52 -0400

    Release 2.1.1 (#50)
    * Updated the pricing on the level 2 and 3 pages. Updated the navbar program names. Edited some of the query names to match contentful
    
    * Added edits for body , included adn not included being empty
    
    Co-authored-by: Rishi <rkhan@cfs.energy>

--- src/components/LevelThree/LevelInfo.js
+++ src/components/LevelThree/LevelInfo.js
@@ -10,14 +10,14 @@ const LevelInfo = () => {
       title: { title },
       price,
       programDates: { heading: programHeading, data: programDates },
-      included: { included },
-      notIncluded: { notIncluded },
       image,
     },
   } = useLongTermData();
 
   const note = useLongTermData()?.infoCard?.note?.note;
   const priceImage = useLongTermData()?.infoCard?.priceImage;
+  const included = useLongTermData()?.infoCard?.included?.included;
+  const notIncluded = useLongTermData()?.infoCard?.notIncluded?.notIncluded;
 
   const data = {
     title,
